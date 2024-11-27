65afa765b3302854a09af0c8e3257c5530a748db
patricio-dsgn <patricio.ngg2@gmail.com>
2024-11-27 02:36:46 -0300

    add draw

--- src/app/components/panel-educacion-nee/panel-educacion-nee.component.ts
+++ src/app/components/panel-educacion-nee/panel-educacion-nee.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
 
+// Importa los componentes necesarios
+import { ElemDraw01Component } from '../elem-draw-01/elem-draw-01.component';
+import { ElemDraw02Component } from '../elem-draw-02/elem-draw-02.component';
+import { ElemDraw03Component } from '../elem-draw-03/elem-draw-03.component';
+import { ElemDraw04Component } from '../elem-draw-04/elem-draw-04.component';
+import { ElemDraw05Component } from '../elem-draw-05/elem-draw-05.component';
+
 @Component({
   selector: 'app-panel-educacion-nee',
   standalone: true,
-  imports: [],
+  imports: [
+    ElemDraw01Component,
+    ElemDraw02Component,
+    ElemDraw03Component,
+    ElemDraw04Component,
+    ElemDraw05Component,
+  ],
   templateUrl: './panel-educacion-nee.component.html',
-  styleUrl: './panel-educacion-nee.component.css'
+  styleUrls: ['./panel-educacion-nee.component.css']
 })
-export class PanelEducacionNeeComponent {
-
-}
+export class PanelEducacionNeeComponent {}
