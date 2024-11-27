276733ddb11c18e90ba80fd20ea9aca36caf2f78
patricio-dsgn <patricio.ngg2@gmail.com>
2024-11-26 21:29:10 -0300

    add home

--- src/app/components/elem-header/elem-header.component.ts
+++ src/app/components/elem-header/elem-header.component.ts
@@ -7,9 +7,10 @@ import { Component, Input } from '@angular/core';
   templateUrl: './elem-header.component.html',
   styleUrl: './elem-header.component.css'
 })
+
 export class ElemHeaderComponent {
-  @Input() htitle: string = '';
-  @Input() hsubtitle: string = '';
+  @Input() eh_title: string = 'Titulo_ts';
+  @Input() eh_subtitle: string = 'Subtitulo_ts';
+  @Input() eh_bg_color: string = '#b2b2b2';
+  @Input() eh_text_color: string = '#ffffff';
 }
-
-
