e62e1b6134563fbf00bf81bd7087b9b2e892b98a
patricio-dsgn <patricio.ngg2@gmail.com>
2024-11-28 16:58:34 -0300

    ok

--- src/app/components/view-home/view-home.component.ts
+++ src/app/components/view-home/view-home.component.ts
@@ -5,6 +5,8 @@ import { ElemCardGrid2Component } from '../elem-card-grid2/elem-card-grid2.compo
 import { ElemCardGrid3Component } from '../elem-card-grid3/elem-card-grid3.component';
 import { ElemButtonComponent } from '../elem-button/elem-button.component';
 import { Router } from '@angular/router';
+import { AuthService } from '../../services/login.services'; // Ajusta la ruta según tu estructura
+
 
 @Component({
   selector: 'app-view-home',
@@ -19,20 +21,27 @@ import { Router } from '@angular/router';
   templateUrl: './view-home.component.html',
   styleUrl: './view-home.component.css'
 })
+
+
 export class ViewHomeComponent {
 
-  constructor(private router: Router) {}
-
+  constructor(private router: Router, private authService: AuthService) {}
+  
   // Navegación programática
   navigateTo(path: string): void {
     this.router.navigate([`/${path}`]);
   }
+  
+  // Método para obtener los datos del usuario
+  getUsuarioData(correo: string): void {
+    this.authService.getUsuario(correo).subscribe({
+      next: (response) => {
+        console.log('-------------')
+        console.log('Datos del usuario:', response.data); // Muestra los datos del usuario
+      },
+      error: (err) => {
+        console.error('Error al obtener los datos del usuario:', err);
+      }
+    });
+  }
 }
-
-
-
-
-
-
-
-
